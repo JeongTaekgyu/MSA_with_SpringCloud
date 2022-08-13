fba50d2b1f08ecebfdd6e14768ba820bd6645d18
taekgyu <taekgyu0602@gmail.com>
2022-08-13 14:32:03 +0900

    yml으로 request, response Filter 적용

--- APIgatewayService/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ APIgatewayService/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
     // application.yml 에서 했던 동작을 java로 한다.
-    @Bean
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
