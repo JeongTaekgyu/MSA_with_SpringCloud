6c7db872e81225ef69d7825f6e92fe871f41035f
taekgyu <taekgyu0602@gmail.com>
2022-08-13 21:20:33 +0900

    유레카사용, Load Balancer 이용한 랜덤포트 방식 (라운드로빈으로 요청된다.)

--- firstService/src/main/java/com/example/firstservice/FirstServiceController.java
+++ firstService/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,27 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+
+    Environment env; // 설정값들을 불러올 수 있는 클래스
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +35,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port ={}", request.getServerPort());   // request로 값을 불러올 수 있고
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s"
+                ,env.getProperty("local.server.port")); // Environment 클래스로 값을 불러올 수 있다.
     }
 }
