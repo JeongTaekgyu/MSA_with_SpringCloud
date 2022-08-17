dc52f075e023ba516790cea6bf9ed2af276f7948
taekgyu <taekgyu0602@gmail.com>
2022-08-17 23:40:48 +0900

    사용자 추가(회원가입) 구현

--- sampleApplication/src/main/java/com/example/userservice/controller/UserController.java
+++ sampleApplication/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,24 +1,32 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
     private Environment env;
+    private UserService userService;
 
     @Autowired  // 권장하지 않는 방법 그냥 간단한 예제니까 한다.
     private Greeting greeting;
 
     @Autowired
-    public UserController(Environment env) {
+    public UserController(Environment env, UserService userService) {
         this.env = env;
+        this.userService = userService;
     }
 
     @GetMapping("/health_check")
@@ -31,5 +39,20 @@ public class UserController {
         // yml 파일에 있는 내용 출력 2가지 방법
         //return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users") // 제네릭타입으로 반환할 형태도 명시할 수 있다.
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        // userDto 를 ResponseUser 형으로 변경한다.
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        // rest api 식으로 반환하자
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser); // 201번 성공코드 반환
     }
 }
