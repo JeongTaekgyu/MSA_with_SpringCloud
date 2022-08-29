32611119d10c9c67f447b1f68ffefa90c78eca24
taekgyu <taekgyu0602@gmail.com>
2022-08-29 17:54:52 +0900

    RestTemplate으로 msa간 통신

--- sampleApplication/src/main/java/com/example/userservice/UserServiceApplication.java
+++ sampleApplication/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient  // 유레카 서버에 클라이언트를 등록한다.
@@ -18,4 +20,11 @@ public class UserServiceApplication {
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean   // @Bean으로 등록하면 컨트롤러, 서비스, 레포지토리에서 주입받아서 사용가능하다.
+    @LoadBalanced   //
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate(); // 반환 시키고자 하는 데이터의 종류를 RestTemplate이라고 명시
+    }
+
 }
--- sampleApplication/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ sampleApplication/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,12 +7,17 @@ import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +28,9 @@ public class UserServiceImpl implements UserService{
 
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
+
+    Environment env;
+    RestTemplate restTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -36,9 +44,14 @@ public class UserServiceImpl implements UserService{
     }
 
     @Autowired  // 생성자를 통해 빈을 주입
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+    public UserServiceImpl(UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder,
+                           Environment env,
+                           RestTemplate restTemplate){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.env = env;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -68,9 +81,22 @@ public class UserServiceImpl implements UserService{
             throw new UsernameNotFoundException("User not found");
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+//        List<ResponseOrder> orders = new ArrayList<>();
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+        /* Using as RestTemplate */
+        // 호출하고자하는 orderUrl
+        String orderUrl = String.format(env.getProperty("order_service.url"), userId);  // order_service에 파라미터가 있어서 format으로 치환함
+
+        // 주소값, 호출하고자하는 메서드의 타입, 요청할때 파라미터, 어떤 형식으로 전달받을 것인지
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                                        new ParameterizedTypeReference<List<ResponseOrder>>() {
+                }); // 이걸 호출하니까 order-service의
+        // orderUrl = http://127.0.0.1:8000/order-service/{userId}/orders 로 넘어가네
+        // 그리고 order-service에서 반환하면 다시 여기로 온다.
+        List<ResponseOrder> orderList = orderListResponse.getBody();
+
+        userDto.setOrders(orderList);
 
         return userDto;
     }
