e22403e8ced25b88f157585ecbc57ad269871f79
taekgyu <taekgyu0602@gmail.com>
2022-09-01 20:02:10 +0900

    KafkaProducer 구현

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.messagequeue.KafkaProducer;
 import com.example.orderservice.repository.OrderEntity;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
@@ -22,11 +23,13 @@ public class OrderController {
 
     Environment env;
     OrderService orderService;
+    KafkaProducer kafkaProducer;
 
     @Autowired
-    public OrderController(Environment env, OrderService orderService) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer) {
         this.env = env;
         this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
     }
 
     @GetMapping("/health_check")
@@ -42,13 +45,19 @@ public class OrderController {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
+        /* jpa */
         // RequestUser를 UserDto로 매핑한다.
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
-        OrderDto createdOrder = orderService.createOrder(orderDto);
+        OrderDto createdOrder = orderService.createOrder(orderDto); // 주문함
 
         // userDto 를 ResponseUser 형으로 변경한다.
         ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        // 카프카에 메시지(주문서비스)를 전달하는 작업을 추가로 넣음
+        /* send this order to the kafka */
+        kafkaProducer.send("example-catalog-topic", orderDto);
+
 
         // rest api 식으로 반환하자
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder); // 201번 성공코드 반환
---
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
@@ -0,0 +1,39 @@
+package com.example.orderservice.messagequeue;
+
+import com.example.orderservice.dto.OrderDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public OrderDto send(String topic, OrderDto orderDto) {
+        // 주문 서비스를 json 포맷으로 변경
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "";
+        try {
+            // String으로 전달하는데 이것을 json 형태를 가질 수 있도록 mapper 기능을 사용함
+            jsonInString = mapper.writeValueAsString(orderDto);
+        } catch (JsonProcessingException ex){
+            ex.printStackTrace();
+        }
+
+        kafkaTemplate.send(topic, jsonInString);
+        // kafka에서 메시지가 잘 보내졌다고 로그 찍음
+        log.info("kafka Producer sent data from the Order microservice: " + orderDto);
+
+        return orderDto;
+    }
+}
